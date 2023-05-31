07fd409d83f4274ff1956909a5c52d9ba5b79ffd
Renan Ferraz <renanferraz.rw@hotmail.com>
2023-05-31 13:29:52 -0300

    Aula Icones

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, TouchableOpacity } from 'react-native';
+import { Feather } from 'react-native-vector-icons';
+import LottieView from 'lottie-react-native';
+import React from 'react';
 
 export default function App() {
+  let animation = React.createRef();
+
+  function startAnimation(){
+    animation.current.play();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={startAnimation}>
+        <Feather name="play" color="red" size={60} />
+      </TouchableOpacity>
+      <LottieView
+        source={require('./assets/check.json')} 
+        style={{
+          width: 200,
+          height: 200.
+        }}
+        loop={false}
+        ref={animation}
+      />
+      <Button title="Start Animation" onPress={startAnimation} />
       <StatusBar style="auto" />
     </View>
   );
